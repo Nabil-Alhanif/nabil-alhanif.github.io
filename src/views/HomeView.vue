35bbb67f17e98c396a6b3c0d96b89b96e5a4ea68
Nabil-Alhanif <nabil.alhanif@protonmail.com>
2024-06-29 15:45:07 -0700

    Migration to vue

---
+++ src/views/HomeView.vue
@@ -0,0 +1,14 @@
+<template>
+	<main>
+		<a href="https://nabil-alhanif.github.io/portofolio/">My Portofolio</a>
+		<h1 style="color: indigo">This is a Heading</h1>
+		<h2>This is a smaller heading</h2>
+		<h3>This is an even smaller heading</h3>
+		<h4>This is an even more smaller heading</h4>
+		<h5>And this is even more more smaller heading</h5>
+		<h6>Now this is finally the smallest heading</h6>
+		<p>This is a paragraph.</p>
+		<hr />
+		<p>This is another paragraph.</p>
+	</main>
+</template>
