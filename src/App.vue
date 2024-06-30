8263bcfff9670fa124523dac94ab1e847a7c1982
Nabil-Alhanif <nabil.alhanif@protonmail.com>
2024-06-30 16:29:57 -0700

    Add code to toggle navbar in small screen

--- src/App.vue
+++ src/App.vue
@@ -7,10 +7,16 @@
 </template>
 
 <script setup>
+import { onMounted } from 'vue'
 import { RouterView } from 'vue-router'
+import { initFlowbite } from 'flowbite'
 
 import TheNavbar from '@/components/TheNavbar.vue'
 import TheFooter from '@/components/TheFooter.vue'
+
+onMounted(() => {
+	initFlowbite()
+})
 </script>
 
 <style scoped></style>
