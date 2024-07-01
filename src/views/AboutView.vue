4522c3e1fb070997cff2094b9721394e1f93f830
Nabil-Alhanif <nabil.alhanif@protonmail.com>
2024-07-01 00:40:56 -0700

    Update credits and about page

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,6 +1,23 @@
 <template>
-	<main>
-		<h1>About</h1>
+	<main class="flex flex-col bg-gray-100 p-6">
+		<div class="format mx-auto w-full rounded-lg bg-white p-8 shadow-md">
+			<h1 class="mb-6 text-3xl font-bold">About</h1>
+			<p class="lead">Hi there! Welcome to my corner of the internet!</p>
+			<p>
+				In all honesty, I'm not really sure what I should write here. But yeah, about me, I'm a
+				student from Indonesia, and I'm currently studying Chemical & Biological Engineering in UBC
+				Vancouver. My main interests is in biotechnology & bioinformatics, especially with plant
+				biotechnology. As of lately, I'm also interested in the use of computer simulation in
+				biotech.
+			</p>
+			<p>
+				Other than that, I'm a big fan of linux and I love tinkering with anything I can get my
+				hands on. I'm currently learning about Vue JS and web development (which is why I made this
+				website), because it's the summer and I don't really have anything better to do
+				unfortunately.
+			</p>
+			<p>So yeah, nice to meet you, and I hope you have a good time going through my website :D</p>
+		</div>
 	</main>
 </template>
 
--- src/views/CreditsView.vue
+++ src/views/CreditsView.vue
@@ -1,15 +1,32 @@
 <template>
-	<main>
-		<h1>Credit</h1>
-		<p>
-			Made with <a href="https://vuejs.org" target="_blank">Vue JS</a> and
-			<a href="https://flowbite.com" target="_blank">Flowbite</a>
-		</p>
-		<p>
-			Icons from
-			<a href="https://icons.getbootstrap.com/" target="_blank">Bootstrap</a>
-			and <a href="https://flowbite.com/icons/" target="_blank">Flowbite</a>
-		</p>
+	<main class="flex flex-col items-center justify-center bg-gray-100 p-6">
+		<div class="max-w-lg rounded-lg bg-white p-8 text-center shadow-md">
+			<h1 class="mb-6 font-body text-3xl">Credit</h1>
+			<p class="mb-4">
+				Made with
+				<a href="https://vuejs.org" target="_blank" class="text-blue-500 hover:underline">Vue JS</a>
+				and
+				<a href="https://flowbite.com" target="_blank" class="text-blue-500 hover:underline"
+					>Flowbite</a
+				>
+			</p>
+			<p class="mb-4">
+				Icons from
+				<a
+					href="https://icons.getbootstrap.com/"
+					target="_blank"
+					class="text-blue-500 hover:underline"
+					>Bootstrap</a
+				>
+				and
+				<a href="https://flowbite.com/icons/" target="_blank" class="text-blue-500 hover:underline"
+					>Flowbite</a
+				>
+			</p>
+			<p class="mt-6 text-gray-600">
+				This website wouldn't be possible without these amazing open-source project & communities.
+			</p>
+		</div>
 	</main>
 </template>
 
