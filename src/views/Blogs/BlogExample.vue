92b042e2be19015cda054ae1c4079f2adf4a227b
Nabil-Alhanif <nabil.alhanif@protonmail.com>
2024-06-30 19:12:38 -0700

    Improve date formatting for Blog posts

--- src/views/Blogs/BlogExample.vue
+++ src/views/Blogs/BlogExample.vue
@@ -8,16 +8,19 @@
 					<address class="mb-6 flex items-center not-italic">
 						<div class="mr-3 inline-flex items-center text-sm text-gray-900 dark:text-white">
 							<div
-								class="relative mr-4 size-12 overflow-hidden rounded-full bg-gray-100 dark:bg-gray-600"
+								class="relative mr-4 size-16 overflow-hidden rounded-full bg-gray-100 dark:bg-gray-600"
 							>
-								<UserAvatarIcon class="absolute size-14 text-gray-400" />
+								<UserAvatarIcon class="absolute size-20 text-gray-400" />
 							</div>
 							<div>
 								<a href="#" rel="author" class="text-xl font-bold text-gray-900 dark:text-white"
 									>Test Author</a
 								>
 								<p class="text-base text-gray-500 dark:text-gray-400">
-									<time pubdate datetime="2022-02-08" title="February 8th, 2022">Feb. 8, 2022</time>
+									<DateComponent :iso-date-string="created_at" message="Created on: " />
+								</p>
+								<p class="text-base text-gray-500 dark:text-gray-400">
+									<DateComponent :iso-date-string="edited_at" message="Last edited on: " />
 								</p>
 							</div>
 						</div>
@@ -170,4 +173,8 @@
 
 <script setup>
 import UserAvatarIcon from '@/components/icons/UserAvatarIcon.vue'
+import DateComponent from '@/components/DateComponent.vue'
+
+const created_at = '2024-07-01T00:41:00.000Z'
+const edited_at = '2024-07-01T00:41:00.000Z'
 </script>
